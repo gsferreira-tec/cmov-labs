46e0598a53cfa59d69c9a715a9cffafd84babef4
root <up201706719@up.pt>
2026-04-28 14:42:44 +0100

    changed this script a bit to plot/save various figures per study in a case by case basis

--- lab4/study-plot.py
+++ lab4/study-plot.py
@@ -26,8 +26,6 @@ def main():
     time.sleep(1)
     print(f"\nGenerating, saving and plotting the graph for study number {study_nr}...\n")
 
-    plt.figure(figsize=(8,5))
-
     if study_nr == 1:
         if "distance_m" not in df1.columns or "rx_kbit_s" not in df1.columns:
             print("CSV1 must contain data for 'distance_m' and 'rx_kbit_s'")
@@ -57,6 +55,12 @@ def main():
             plt.plot(df2["distance_m"], df2["wireshark_kbit_s"], marker="*", linewidth=3, color="red", label="Wireshark")
         plt.xlabel("Distance (m)")
         plt.ylabel("Throughput (Kbps)")
+
+        plt.savefig(out_path, dpi=200) # saving the figure
+
+        pl.tight_layout()
+        plt.legend()
+        plt.show()
 
     elif study_nr == 2:
         if "file" not in df1.columns or "rx_kbit_s" not in df1.columns:
@@ -97,24 +101,73 @@ def main():
         lines2, labels2 = ax2.get_legend_handles_labels()
         ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper right')
 
+        plt.savefig(out_path, dpi=200) # saving the figure
+
+        pl.tight_layout()
+        plt.legend()
+        plt.show()
 
     elif study_nr == 3:
         if "file" not in df1.columns or "rx_kbit_s" not in df1.columns:
             print("CSV1 must contain data for 'file' and 'rx_kbit_s'")
             sys.exit(1)
 
+        out_path_2 = Path("./3rd-study-mean.png")
+        out_path_3 = Path("./3rd-study-pktloss.png")
+
 
         df1["udp_birate"] = pd.to_numeric(df1["file"].str.extract(r"UDP_DataRate=(\d+)", expand=False), errors="coerce")
         df1["rx_kbit_s"] = pd.to_numeric(df1["rx_kbit_s"], errors="coerce")
+        df1["delay_ms"] = pd.to_numeric(df1["delay_ms"], errors="coerce")
+        df1["loss_pct"] = pd.to_numeric(df1["loss_pct"], errors="coerce")
         df1 = df1.dropna(subset=["udp_birate","rx_kbit_s"]).sort_values("udp_birate")
 
         df_tcp = df1[df1["protocol"]=="TCP"]
         df_udp = df1[df1["protocol"]=="UDP"]
 
+        # plotting mean delay and saving
+        plt.figure(figsize=(10,6))
+        plt.plot(df_tcp["udp_birate"], df_tcp["delay_ms"], marker="s", linewidth=3, color="red", label="TCP Mean Delay (ms)")
+        plt.plot(df_udp["udp_birate"], df_udp["delay_ms"], marker="o", linewidth=3, color="steelblue", label="UDP Mean Delay (ms)")
+
+        plt.xlabel("Transmitted UDP Bitrate (Mbps)", fontweight="bold")
+        plt.ylabel("Mean Delay (ms)", fontweight="bold")
+        plt.grid(True, linestyle=":", alpha=0.6)
+
+        plt.savefig(out_path_2, dpi=200) # saving the figure
+
+        plt.tight_layout()
+        plt.legend()
+        # plt.show()
+
+        # plotting the packet loss
+        plt.figure(figsize=(10,6))
+        plt.plot(df_udp["udp_birate"], df_udp["loss_pct"], marker="o", linewidth=3, color="steelblue", label="UDP Packet Loss")
+
+        plt.xlabel("Transmitted UDP Bitrate (Mbps)", fontweight="bold")
+        plt.ylabel("Packet Loss Ratio (%)", fontweight="bold")
+        plt.grid(True, linestyle=":", alpha=0.6)
+
+        plt.savefig(out_path_3, dpi=200) # saving the figure
+
+        plt.tight_layout()
+        plt.legend()
+        # plt.show()
+
+        # plotting the UDP vs TCP throughput with varying bitrates
+        plt.figure(figsize=(10,6))
         plt.plot(df_udp["udp_birate"], df_udp["rx_kbit_s"], marker="o", linewidth=3, color="steelblue", label="UDP")
         plt.plot(df_tcp["udp_birate"], df_tcp["rx_kbit_s"], marker="o", linewidth=3, color="red", label="TCP")
-        plt.xlabel("Transmitted UDP Bitrate (Mbps)")
-        plt.ylabel("Throughput (Kbps)")
+
+        plt.xlabel("Transmitted UDP Bitrate (Mbps)", fontweight="bold")
+        plt.ylabel("Throughput (Kbps)", fontweight="bold")
+        plt.grid(True, linestyle=":", alpha=0.6)
+
+        plt.savefig(out_path, dpi=200) # saving the figure
+
+        plt.tight_layout()
+        plt.legend()
+        plt.show()
 
     elif study_nr == 4:
         if "distance_m" not in df1.columns or "rx_kbit_s" not in df1.columns:
@@ -148,18 +201,24 @@ def main():
         plt.ylabel("Throughput (Kbps)")
 
 
+        plt.savefig(out_path, dpi=200) # saving the figure
+
+        pl.tight_layout()
+        plt.legend()
+        plt.show()
+
 
     else:
         print(f"The study number provided {study_nr} is not supported. Aborting...")
         time.sleep(1)
         sys.exit(1)
 
-    if study_nr != 2:
-        plt.grid(True)
-        plt.legend()
-
-    plt.tight_layout()
-    plt.savefig(out_path, dpi=200)
+#     if study_nr != 2:
+#         plt.grid(True)
+#         plt.legend()
+#
+    # plt.tight_layout()
+    # plt.savefig(out_path, dpi=200)
 
     if csv2:
         print(f"The plot for {csv1}_vs_{csv2} was saved in {out_path}")
@@ -168,8 +227,8 @@ def main():
 
     time.sleep(1)
 
-    if study_nr != 2:
-        plt.show()
+    # if study_nr != 2:
+    #     plt.show()
 
 
 if __name__ == "__main__":
