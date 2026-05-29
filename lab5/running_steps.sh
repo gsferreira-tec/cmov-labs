6ee457b1a847976c1d5f4fc4a11e259af06b102a
up202205302 <up202205302@g.uporto.pt>
2026-05-29 23:44:46 +0100

    Contains the Task 3 solution but only for 100MHz

--- lab5/running_steps.sh
+++ lab5/running_steps.sh
@@ -22,30 +22,33 @@ tmux kill-session -t ue 2>/dev/null
 tmux kill-session -t ue1 2>/dev/null
 tmux kill-session -t ue2 2>/dev/null
 
+
 if [ "$1" == "gnb" ]; then 
 
-	cd ~/oai/cmake_targets/ran_build/build
-
-	echo "[*] Starting gNB"
-	tmux new-session -d -s gnb \
-	"sudo ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.usrpb210.conf --gNBs.[0].min_rxtxtime 6 --rfsim --sa"
-
-	sleep 10
 
 	########################
 	#        TASK 1        #
 	########################
+
+	cd ~/oai/cmake_targets/ran_build/build
+
 	
 	if [ "$2" == "1" ] || [ "$2" == "one" ]; then
+
+		echo "+----------------------+"
+		echo "|        TASK 1        |"
+		echo "+----------------------+"
+
+		echo "[*] Starting gNB"
+		tmux new-session -d -s gnb \
+			"sudo ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.usrpb210.conf --gNBs.[0].min_rxtxtime 6 --rfsim --sa"
+
+		sleep 10
+
 		echo "[*] Starting UE"
 
 		tmux new-session -d -s ue \
 	        "sudo ./nr-uesoftmodem -r 106 --numerology 1 --band 78 -C 3619200000 --rfsim --sa --uicc0.imsi 001010000000001 --rfsimulator.serveraddr 127.0.0.1"
-
-
-                #cd ~/oai/ci-scripts/yaml_files/5g_rfsimulator/docker-compose.yaml
-		#echo "[*] Bringing up oai-nr-ue1"
-		#docker compose -f docker-compose.yaml up -d oai-nr-ue1
 
 		sleep 10
 
@@ -60,7 +63,6 @@ if [ "$1" == "gnb" ]; then
 
 		echo "[*] Pinging Downlink 10 times"
 		ssh -t mobile@${IP_HOST_CORE} "sudo docker exec oai-ext-dn ping -c 10 $IP_UE"
-		#sshpass -p 'mobile' ssh -o StrictHostKeyChecking=no mobile@${IP_HOST_CORE} "sudo docker exec oai-ext-dn ping -c 10 $IP_UE" # To instantly pass the password login
 
 		
 
@@ -68,6 +70,16 @@ if [ "$1" == "gnb" ]; then
 	#        TASK 2        #
 	########################
 	elif [ "$2" == "2" ] || [ "$2" == "two" ]; then
+
+		echo "+----------------------+"
+		echo "|        TASK 2        |"
+		echo "+----------------------+"
+
+		echo "[*] Starting gNB"
+		tmux new-session -d -s gnb \
+			"sudo ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.usrpb210.conf --gNBs.[0].min_rxtxtime 6 --rfsim --sa"
+
+		sleep 10
 
 		chmod +x ~/multi-ue.sh  
 
@@ -112,39 +124,117 @@ if [ "$1" == "gnb" ]; then
 	#        TASK 3        #
 	########################
 	
-	elif [ "$3" == "3" ] || [ "$3" == "three" ]; then
+	elif [ "$2" == "3" ] || [ "$2" == "three" ]; then
 
-		cp "$CONF_PATH" "$CONF_TMP"
+		echo "+----------------------+"
+		echo "|        TASK 3        |"
+		echo "+----------------------+"
 
-		echo "[*] Changing gNB settings to 3500MHz center frequency and 100MHz bandwidth"
+		cp "$CONF_PATH" "$CONF_PATH_TMP"
+
+		if [ "$3" == "100" ]; then
+
+			echo "[*] Changing gNB settings to 3500MHz center frequency and 100MHz bandwidth"
+			
+			sed -i '/#/!s/^\([[:space:]]*absoluteFrequencySSB[[:space:]]*=[[:space:]]*\).*$/\1 630048;/' "$CONF_PATH_TMP" # absoluteFrequencySSB = 630000
+
+			sed -i '/#/!s/^\([[:space:]]*dl_absoluteFrequencyPointA[[:space:]]*=[[:space:]]*\).*$/\1 628776;/' "$CONF_PATH_TMP" # dl_absoluteFrequencyPointA = 626724. Had to change to 628776 because OAI doesn't support this value. even though 3GPP does
+
+			sed -i '/#/!s/^\([[:space:]]*dl_frequencyBand[[:space:]]*=[[:space:]]*\).*$/\1 78;/' "$CONF_PATH_TMP"  # dl_frequencyBand = 78
+
+			sed -i '/#/!s/^\([[:space:]]*dl_subcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"  # dl_subcarrierSpacing = 1
+
+			sed -i '/#/!s/^\([[:space:]]*dl_carrierBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 106;/' "$CONF_PATH_TMP"  # dl_carrierBandwidth = 106 
+
+			sed -i '/#/!s/^\([[:space:]]*initialDLBWPlocationAndBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 28875;/' "$CONF_PATH_TMP"  # initialDLBWPlocationAndBandwidth = 1099. Had to change to 28875 because OAI doesn't support this value. even though 3GPP does
+
+
+			sed -i '/#/!s/^\([[:space:]]*initialDLBWPsubcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"  # initialDLBWPsubcarrierSpacing = 1
+
+			sed -i '/#/!s/^\([[:space:]]*initialDLBWPcontrolResourceSetZero[[:space:]]*=[[:space:]]*\).*$/\1 11;/' "$CONF_PATH_TMP"  # CORESET0
+
+			sed -i '/#/!s/^\([[:space:]]*ul_frequencyBand[[:space:]]*=[[:space:]]*\).*$/\1 78;/' "$CONF_PATH_TMP"  # ul_frequencyBand = 78
+
+			sed -i '/#/!s/^\([[:space:]]*ul_carrierBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 106;/' "$CONF_PATH_TMP"  # ul_carrierBandwidth = 106
+
+			sed -i '/#/!s/^\([[:space:]]*initialULBWPlocationAndBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 28875;/' "$CONF_PATH_TMP"  # initialULBWPlocationAndBandwidth = 1099
+
+			sed -i '/#/!s/^\([[:space:]]*initialULBWPsubcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"  # initialULBWPsubcarrierSpacing = 1
 		
-		sed -i '/#/!s/^\([[:space:]]*absoluteFrequencySSB[[:space:]]*=[[:space:]]*\).*$/\1 6300000;/' "$CONF_PATH_TMP"  # absoluteFrequencySSB = 630000
+		elif [ "$3" == "20" ]; then			
 
-		sed -i '/#/!s/^\([[:space:]]*dl_absoluteFrequencyPointA[[:space:]]*=[[:space:]]*\).*$/\1 626724;/' "$CONF_PATH_TMP"  # dl_absoluteFrequencyPointA = 626724
+			echo "20MHz bandwidth doesn't work"
+			exit 1
 
-		sed -i '/#/!s/^\([[:space:]]*dl_frequencyBand[[:space:]]*=[[:space:]]*\).*$/\1 78;/' "$CONF_PATH_TMP"  # dl_frequencyBand = 78
+#			echo "[*] Changing gNB settings to 3500MHz center frequency and 20MHz bandwidth"
+#
+#			sed -i '/#/!s/^\([[:space:]]*absoluteFrequencySSB[[:space:]]*=[[:space:]]*\).*$/\1 630048;/' "$CONF_PATH_TMP"             # absoluteFrequencySSB = 630000
+#
+#			sed -i '/#/!s/^\([[:space:]]*dl_absoluteFrequencyPointA[[:space:]]*=[[:space:]]*\).*$/\1 629388;/' "$CONF_PATH_TMP"       # dl_absoluteFrequencyPointA = 626724
+#
+#			sed -i '/#/!s/^\([[:space:]]*dl_frequencyBand[[:space:]]*=[[:space:]]*\).*$/\1 78;/' "$CONF_PATH_TMP"                    # dl_frequencyBand = 78
+#
+#			sed -i '/#/!s/^\([[:space:]]*dl_subcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"                 # dl_subcarrierSpacing = 1 
+#
+#			sed -i '/#/!s/^\([[:space:]]*dl_carrierBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 51;/' "$CONF_PATH_TMP"                 # dl_carrierBandwidth = 66
+#
+#			sed -i 's/^\([[:space:]]*initialDLBWPlocationAndBandwidth[[:space:]]*=[[:space:]]*\)[0-9]*/\1 13750/' "$CONF_PATH_TMP"  # initialDLBWPlocationAndBandwidth = 4700 
+#
+#			sed -i '/#/!s/^\([[:space:]]*initialDLBWPsubcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"        # initialDLBWPsubcarrierSpacing = 1
+#
+#			sed -i '/#/!s/^\([[:space:]]*initialDLBWPcontrolResourceSetZero[[:space:]]*=[[:space:]]*\).*$/\1 17;/' "$CONF_PATH_TMP"  # CORESET0. Had to change to 17 because OAI doesn't support this value. even though 3GPP does
+#
+#			sed -i '/#/!s/^\([[:space:]]*ul_frequencyBand[[:space:]]*=[[:space:]]*\).*$/\1 78;/' "$CONF_PATH_TMP"                    # ul_frequencyBand = 78
+#
+#			sed -i '/#/!s/^\([[:space:]]*ul_carrierBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 51;/' "$CONF_PATH_TMP"                 # ul_carrierBandwidth = 66
+#
+#			sed -i 's/^\([[:space:]]*initialULBWPlocationAndBandwidth[[:space:]]*=[[:space:]]*\)[0-9]*/\1 13750/' "$CONF_PATH_TMP"  # initialULBWPlocationAndBandwidth = 4700 
+#
+#			sed -i '/#/!s/^\([[:space:]]*initialULBWPsubcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"        # initialULBWPsubcarrierSpacing = 1
 
-		sed -i '/#/!s/^\([[:space:]]*dl_subcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"  # dl_subcarrierSpacing = 1 (30 kHz)
+		else 
+			echo "For the third argument use either 20 (for 20MHz bandwidth) or 100 (for 100MHz bandwidth)"
+			exit 1
+		fi
 
-		sed -i '/#/!s/^\([[:space:]]*dl_carrierBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 106;/' "$CONF_PATH_TMP"  # dl_carrierBandwidth = 106 (100 MHz n78)
+		echo "[*] Starting gNB"
 
-		sed -i '/#/!s/^\([[:space:]]*initialDLBWPlocationAndBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 28875;/' "$CONF_PATH_TMP"  # initialDLBWPlocationAndBandwidth = 28875
+		tmux new-session -d -s gnb \
+    		"sudo ./nr-softmodem -O ${CONF_PATH_TMP} --gNBs.[0].min_rxtxtime 6 --rfsim --sa 2>&1 | stdbuf -oL tee /tmp/gnb_task3_full.log"
 
-		sed -i '/#/!s/^\([[:space:]]*initialDLBWPsubcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"  # initialDLBWPsubcarrierSpacing = 1
+		tmux capture-pane -t gnb -p -S -3000 > /tmp/gnb_task3_full.log 
+		
 
-		sed -i '/#/!s/^\([[:space:]]*initialDLBWPcontrolResourceSetZero[[:space:]]*=[[:space:]]*\).*$/\1 12;/' "$CONF_PATH_TMP"  # CORESET0
+		sleep 10
 
-		sed -i '/#/!s/^\([[:space:]]*ul_frequencyBand[[:space:]]*=[[:space:]]*\).*$/\1 78;/' "$CONF_PATH_TMP"  # ul_frequencyBand = 78
+		head -55 /tmp/gnb_task3_full.log > /tmp/gnb_task3_startup.log 
 
-		sed -i '/#/!s/^\([[:space:]]*ul_carrierBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 106;/' "$CONF_PATH_TMP"  # ul_carrierBandwidth = 106
+		echo "[*] Starting UE"
 
-		sed -i '/#/!s/^\([[:space:]]*initialULBWPlocationAndBandwidth[[:space:]]*=[[:space:]]*\).*$/\1 28875;/' "$CONF_PATH_TMP"  # initialULBWPlocationAndBandwidth = 28875
+		if [ "$3" == "100" ]; then
+			tmux new-session -d -s ue \
+				"sudo ./nr-uesoftmodem -r 106 --numerology 1 --band 78 -C 3450720000 --rfsim --sa --uicc0.imsi 001010000000001 --rfsimulator.serveraddr 127.0.0.1"
+		elif [ "$3" == "20" ]; then
+			echo "20MHz bandwidth doesn't work"
+			exit 1
+			#tmux new-session -d -s ue \
+			#	"sudo ./nr-uesoftmodem -r 51 --numerology 1 --band 78 -C 3450720000 --rfsim --sa --uicc0.imsi 001010000000001 --rfsimulator.serveraddr 127.0.0.1" # Adjusted to 3450.72 MHz
+		fi
 
-		sed -i '/#/!s/^\([[:space:]]*initialULBWPsubcarrierSpacing[[:space:]]*=[[:space:]]*\).*$/\1 1;/' "$CONF_PATH_TMP"  # initialULBWPsubcarrierSpacing = 1
+		sleep 10
 
+		echo "[*] Pinging Uplink 10 times"
+		ping -c 10 $IP_EXT_DN -I oaitun_ue1 
 
-	else 
-		echo "Both gNB and UE were started"
+		IP_UE=$(ip addr show oaitun_ue1 | grep "inet " | awk '{print $2}' | cut -d/ -f1)
+
+		echo "IP of UE: $IP_UE"
+
+		sleep 5
+
+		echo "[*] Pinging Downlink 10 times"
+		ssh -t mobile@${IP_HOST_CORE} "sudo docker exec oai-ext-dn ping -c 10 $IP_UE"
+
 	fi
 
 
@@ -163,7 +253,7 @@ elif [ "$1" == "core" ]; then
 	if [ "$2" == "1" ] || [ "$2" == "one" ]; then
 		sudo rm ${FILE_PATH}dl-ul-pings-task1.pcap
 
-		echo "[*] Listening for ${IP_EXT_DN} IP for Task 1 communicatin with one UE"
+		echo "[*] Listening for ${IP_EXT_DN} IP for Task 1 communicating with one UE"
 		sudo timeout 120 tcpdump -i any "udp port 2152 or host ${IP_EXT_DN}" -U -w ${FILE_PATH}dl-ul-pings-task1.pcap
 
 
@@ -173,7 +263,7 @@ elif [ "$1" == "core" ]; then
 	elif [ "$2" == "2" ] || [ "$2" == "two" ]; then
 		sudo rm ${FILE_PATH}dl-ul-pings-task2.pcap
 
-		echo "[*] Listening for ${IP_EXT_DN} IP for Task 2 communicatin with UE1 and then UE2"
+		echo "[*] Listening for ${IP_EXT_DN} IP for Task 2 communicating with UE1 and then UE2"
 		sudo timeout 180 tcpdump -i any "udp port 2152 or host ${IP_EXT_DN}" -U -w ${FILE_PATH}dl-ul-pings-task2.pcap
 	
 	
@@ -182,12 +272,16 @@ elif [ "$1" == "core" ]; then
 	#        TASK 3        #
 	########################
 	
-	elif [ "$3" == "3" ] || [ "$3" == "three" ]; then
-		echo "Empty"
+	elif [ "$2" == "3" ] || [ "$2" == "three" ]; then
+
+		sudo rm ${FILE_PATH}dl-ul-pings-task3.pcap
+
+		echo "[*] Listening for ${IP_EXT_DN} IP for Task 3 communicating with one UE at 100MHz at a 3500MHz"
+		sudo timeout 180 tcpdump -i any "udp port 2152 or host ${IP_EXT_DN}" -U -w ${FILE_PATH}dl-ul-pings-task3.pcap
 
 	fi
 
 else
-	echo "Usage: ./$0 <gnb/core> <(1|one)/(2|two)>"
+	echo "Usage: ./$0 <gnb/core> <(1|one)/(2|two)/(3|three)>"
 	exit 1
 fi
